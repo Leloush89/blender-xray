8e9ef6721385771efec391b93a27acf54e2ec4a2
Vakhurin Sergey <igelbox@gmail.com>
2016-01-17 14:23:39 +0300

    [draft]

--- io_scene_xray/plugin_prefs.py
+++ io_scene_xray/plugin_prefs.py
@@ -1,4 +1,5 @@
 import bpy
+import os.path
 
 
 def get_preferences():
@@ -8,9 +9,43 @@ def get_preferences():
 class PluginPreferences(bpy.types.AddonPreferences):
     bl_idname = 'io_scene_xray'
 
+    def xr_set(self, prop_name):
+        value = getattr(self, prop_name)
+        print(0, prop_name, value)
+        if not value:
+            if prop_name == 'textures_folder':
+                self.gamedata_folder
+            return
+        path = os.path.abspath(os.path.join(value, os.pardir))
+        for pn, ps in {
+            'textures_folder': 'textures',
+            'gamemtl_file': 'gamemtl.xr',
+            'cshader_file': 'shaders.xr',
+            'eshader_file': 'shaders_xrlc.xr'
+        }.items():
+            print(1, pn, ps)
+            if pn == prop_name:
+                continue
+            pv = getattr(self, pn)
+            print(2, pv)
+            if pv:
+                continue
+            pv = os.path.join(path, ps)
+            print(3, pv)
+            if os.path.exists(pv):
+                setattr(self, pn, pv)
+
     gamedata_folder = bpy.props.StringProperty(name='gamedata', description='The path to the \'gamedata\' directory', subtype='DIR_PATH')
-    textures_folder = bpy.props.StringProperty(name='Textures Folder', description='The path to the \'gamedata/textures\' directory', subtype='DIR_PATH')
-    gamemtl_file = bpy.props.StringProperty(name='GameMtl File', description='The path to the \'gamemtl.xr\' file', subtype='FILE_PATH')
+    textures_folder = bpy.props.StringProperty(
+            name='Textures Folder',
+            description='The path to the \'gamedata/textures\' directory',
+            update=lambda self, context: self.xr_set('textures_folder'),
+            subtype='DIR_PATH')
+    gamemtl_file = bpy.props.StringProperty(
+            name='GameMtl File',
+            description='The path to the \'gamemtl.xr\' file',
+            update=lambda self, context: self.xr_set('gamemtl_file'),
+            subtype='FILE_PATH')
     eshader_file = bpy.props.StringProperty(name='EShader File', description='The path to the \'shaders.xr\' file', subtype='FILE_PATH')
     cshader_file = bpy.props.StringProperty(name='CShader File', description='The path to the \'shaders_xrlc.xr\' file', subtype='FILE_PATH')
     expert_mode = bpy.props.BoolProperty(name='Expert Mode', description='Show additional properties/controls')
@@ -25,7 +60,9 @@ class PluginPreferences(bpy.types.AddonPreferences):
     def draw(self, context):
         layout = self.layout
         if not self.textures_folder and self.gamedata_folder:
-            self.textures_folder = self.get_textures_folder()
+            tf = self.get_textures_folder()
+            print('x', tf)
+            self.textures_folder = tf
         layout.prop(self, 'textures_folder', expand=True)
         layout.prop(self, 'gamemtl_file', emboss=True)
         layout.prop(self, 'eshader_file')
