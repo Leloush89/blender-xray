3c495380ee58d05c10f3182826d043d1b8cee3b7
Vakhurin Sergey <igelbox@gmail.com>
2016-05-24 22:45:23 +0300

    [draft]

--- io_scene_xray/xray_envelope.py
+++ io_scene_xray/xray_envelope.py
@@ -60,6 +60,12 @@ def import_envelope(pr, fc, fps, kv, warn=print):
 
 
 def export_envelope(pw, fc, fps, kv, warn=print):
+    def f2q16(v):
+        # print('a', v)
+        v = max(min(v, 32), -32)
+        # print('b', v)
+        return int((v + 32) * 65535 / 64)
+
     b = None
     if fc.extrapolation == 'CONSTANT':
         b = Behaviour.CONSTANT
@@ -70,14 +76,14 @@ def export_envelope(pw, fc, fps, kv, warn=print):
         warn('Envelope: extrapolation {} not supported, replaced with {}'.format(fc.extrapolation, b.name))
     pw.putf('BB', b.value, b.value)
 
-    replace_unsupported_to = Shape.TCB
+    replace_unsupported_to = Shape.BEZIER_2D
     unsupported_occured = set()
     fckf = fc.keyframe_points
     pw.putf('H', len(fckf))
     pkf = None
     for kf in fckf:
         pw.putf('ff', kf.co.y / kv, kf.co.x / fps)
-        sh = Shape.STEPPED
+        sh = Shape.BEZIER_2D
         if pkf:
             if pkf.interpolation == 'CONSTANT':
                 sh = Shape.STEPPED
@@ -89,7 +95,14 @@ def export_envelope(pw, fc, fps, kv, warn=print):
         pw.putf('B', sh.value)
         if sh != Shape.STEPPED:
             pw.putf('HHH', 32768, 32768, 32768)
-            pw.putf('HHHH', 32768, 32768, 32768, 32768)
+            # print(pkf.handle_right if pkf else None, kf.handle_left)
+            # pw.putf('HHHH', f2q16(0), f2q16(0), f2q16(0), f2q16(10))
+            hl = kf.handle_left - kf.co
+            if pkf:
+                hl.x = pkf.co.x + (kf.co.x - pkf.co.x) * (1 / (1 - hl.x)) - kf.co.x
+            hr = kf.handle_right - kf.co
+            print(hl, hr)
+            pw.putf('HHHH', f2q16(hl.x / 30), f2q16(hl.y), f2q16(hr.x / 30), f2q16(hr.y))
         pkf = kf
 
     if len(unsupported_occured):
